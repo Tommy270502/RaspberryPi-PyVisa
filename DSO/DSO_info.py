0ae07088026aa8c8b3eb5cbd1251611b74e94ad4
Thomas S. Perri <87670767+Tommy270502@users.noreply.github.com>
2022-02-03 16:46:51 +0100

    Add files via upload

--- DSO/DSO_info.py
+++ DSO/DSO_info.py
@@ -1,1 +1,9 @@
-
+import pyvisa
+#Create a resource manager
+resources = pyvisa.ResourceManager('@py')
+#Open the DSO by name. (Change this to the string for your instrument)
+oscilloscope = resources.open_resource('YOURINSTRUMENTSTRING')
+#Return the DSO's ID string to tell us it's there
+print(oscilloscope.query('*IDN?'))
+#Close the connection
+oscilloscope.close()
